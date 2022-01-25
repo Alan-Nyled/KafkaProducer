b4734ccfef00c98d8d1468aee2966ecae60b4dad
Kristian Toustrup Langborg-Hansen <ktlh@smartlearning.dk>
2022-01-25 12:52:09 +0100

    Add project files.

---
+++ KafkaProducer/Program.cs
@@ -0,0 +1,13 @@
+﻿using Confluent.Kafka;
+using System.Net;
+
+var config = new ProducerConfig
+{
+    BootstrapServers = "172.19.95.154:9092",
+    ClientId = Dns.GetHostName(),
+};
+
+using (var producer = new ProducerBuilder<Null, string>(config).Build())
+{
+    await producer.ProduceAsync("quickstart-events", new Message<Null, string> { Value = "Her er beskeden " });
+}
